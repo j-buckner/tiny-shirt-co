2cc4ef74f560aaaa1c81685b9ab3bbb3bf8f4edc
Jared Buckner <buckner.jared@gmail.com>
2017-02-28 21:09:43 -0600

    update animations

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -13,14 +13,14 @@ function fadeOut() {
 
 $('#CheckoutWrapper').hide();
 
-var canvas = document.querySelector("canvas");
+var canvas = document.getElementById("canvas1");
 var ctx = canvas.getContext("2d"),
     dashLen = 200, dashOffset = dashLen, speed = 18,
-    txt = "tiny shirt co.", x = (window.innerWidth / 2) - 200, i = 0;
+    txt = "tiny shirt co.", x = (window.innerWidth / 2) - 225, i = 0;
 
 
 canvas.width = window.innerWidth;
-ctx.font = "80px Indie Flower, sans-serif"; 
+ctx.font = "90px Lato, sans-serif"; 
 ctx.lineWidth = 1; ctx.lineJoin = "round"; ctx.globalAlpha = 1;
 ctx.strokeStyle = ctx.fillStyle = "#419bf4";
 
@@ -40,6 +40,49 @@ ctx.strokeStyle = ctx.fillStyle = "#419bf4";
     if (i < txt.length) requestAnimationFrame(loop);
   }
 })();
+
+setTimeout(function (){
+  var canvas = document.getElementById("canvas2");
+  var ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  ctx.font = "45px Lato, sans-serif"; 
+  ctx.lineWidth = 1; ctx.lineJoin = "round"; ctx.globalAlpha = 1;
+  ctx.strokeStyle = ctx.fillStyle = "#419bf4";
+      dashLen = 200, dashOffset = dashLen, speed = 25,
+      txt = "tiny things on normal shirts.", x = (window.innerWidth / 2) - 270, i = 0;
+  (function loop() {
+    ctx.clearRect(x, 0, 60, 150);
+    ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
+    dashOffset -= speed;                                         // reduce dash length
+    ctx.strokeText(txt[i], x, 90);                               // stroke letter
+  console.log('here4');
+    if (dashOffset > 0) requestAnimationFrame(loop);             // animate
+    else {
+      // ctx.fillText(txt[i], x, 90);                               // fill final letter
+      dashOffset = dashLen;                                      // prep next char
+      x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
+      ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random());         // random y-delta
+      ctx.rotate(Math.random() * 0.005);                         // random rotation
+      if (i < txt.length) requestAnimationFrame(loop);
+    }
+  })();
+}, 4000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Create a Stripe client
 var stripe = Stripe('pk_test_DsyPnuAkrgucqixmAvfPA9hO');
