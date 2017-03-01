95df0688886a5dadb748d8da6b2336e4d6f17c5a
Jared Buckner <buckner.jared@gmail.com>
2017-02-28 20:28:36 -0600

    get basic stripe integration

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var checkout = require('./routes/checkout');
+// var checkout = require('./routes/checkout');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/checkout', checkout);
+// app.use('/checkout', checkout);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,5 @@
+Stripe.setPublishableKey('pk_test_DsyPnuAkrgucqixmAvfPA9hO');
+
 $('select.dropdown').dropdown();
 $('.autumn.leaf')
   .transition('AppWrapper')
@@ -38,3 +40,63 @@ ctx.strokeStyle = ctx.fillStyle = "#419bf4";
     if (i < txt.length) requestAnimationFrame(loop);
   }
 })();
+
+// Create a Stripe client
+var stripe = Stripe('pk_test_DsyPnuAkrgucqixmAvfPA9hO');
+
+console.log('woah: ', stripe);
+// Create an instance of Elements
+var elements = stripe.elements();
+
+// Custom styling can be passed to options when creating an Element.
+// (Note that this demo uses a wider set of styles than the guide below.)
+var style = {
+  base: {
+    color: '#32325d',
+    lineHeight: '24px',
+    fontFamily: 'Helvetica Neue',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
+    color: '#fa755a',
+    iconColor: '#fa755a'
+  }
+};
+
+// Create an instance of the card Element
+var card = elements.create('card', {style: style});
+
+// Add an instance of the card Element into the `card-element` <div>
+card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+  const displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Handle form submission
+var form = document.getElementById('SubmitOrder');
+form.addEventListener('click', function(event) {
+  console.log('yay clicked');
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server
+      stripeTokenHandler(result.token);
+    }
+  });
+});
