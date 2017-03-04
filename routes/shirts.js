0ee5d8356bdcf8bd47065727780e48cee43403ad
Jared Buckner <buckner.jared@gmail.com>
2017-03-04 16:33:01 -0600

    add basic prismic integration

--- routes/shirts.js
+++ routes/shirts.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var prismic = require('prismic.io');
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shirts', { title: 'tinyshirtco shirts' });
+  prismic.api("https://tiny-shirt-co.prismic.io/api").then(function(api) {
+    return api.query(prismic.Predicates.at("document.type", "product"));
+  }).then(function(response) {
+    let shirtData = {
+      url: response.results[0].data['product.image'].value.main.url,
+      title: response.results[0].data['product.title'].value[0].text,
+      description: response.results[0].data['product.description'].value[0].text,
+    }
+    res.render('shirts', { data: shirtData });
+  }, function(err) {
+    console.log("Something went wrong: ", err);
+  });
 });
 
 module.exports = router;
